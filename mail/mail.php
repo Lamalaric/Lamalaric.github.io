3f94cbcb83f0656cf3aa26d30be2e8d60b412079
Amalaric <lamalaric93@gmail.com>
2021-10-24 20:28:31 +0200

    Corrections erreur IDE

--- mail/mail.php
+++ mail/mail.php
@@ -1,6 +1,5 @@
 <?php 
 
 $envoi = mail('alf2002@hotmail.fr', "Portfolio: ".$_POST['sujet'], $_POST['message'], $_POST['mail']);
-header('Location: http://perso-etudiant.u-pem.fr/~alefor03/index.html');
+header('Location: https://amalaric.dev');
 
-?>
--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,3 @@
-// window.onscroll = function() {
-    
-//     if (document.documentElement.scrollTop > 50) {
-//         document.getElementById("navbar").style.background = "black";
-//         document.getElementById("navbar").style.padding = "0";
-//         document.getElementById("navbar").style.borderBottom = "1px solid #606060";
-//     } else {
-//         document.getElementById("navbar").style.background = "transparent";
-//         document.getElementById("navbar").style.padding = "0 20px";
-//     }
-// }
-
 window.onload = () => {
     // Ecouteur d'évènement sur scroll
     window.addEventListener("scroll", () => {
@@ -25,45 +13,8 @@ window.onload = () => {
         document.getElementById("progress").style.width = barre+"px"
     })
 }
-(function(){
-    $(document).on('scroll',function(){ // Détection du scroll
-        // Calcul de la hauteur "utile"
-        let hauteur = $(document).height()-$(window).height()
-        // Récupération de la position verticale
-        let position = $(document).scrollTop()
-        // Récupération de la largeur de la fenêtre
-        let largeur = $(window).width()
-        // Calcul de la largeur de la barre     
-        let barre = position / hauteur * largeur
-        // Modification du CSS pour élargir ou réduire la barre     
-        $("#progress").css("width",barre)
-    });
-});
 
 function toggleDivSize(div) {
-    var x = document.getElementsByClassName(div);
+    let x = document.getElementsByClassName(div);
     x[0].classList.toggle('max-div');
 }
-
-// var clicks = 0;
-// function countClick() {
-//     clicks += 1;
-//     console.log(clicks);
-// }
-
-// function maximizeDiv(div) {
-//     if (clicks%2 != 0) {               // Si le nombre de clics est impair, alors la div est censée être petite. Donc on l'aggrandit
-//         var x = document.getElementsByClassName(div);
-//         x[0].style.width="100%";
-//         x[0].style.height="750px";
-//     }
-// }
-
-// function minimizeDiv(div) {
-//     // var element = document.getElementById('maximized');
-//     if (clicks%2 == 0) {               // Si le nombre de clics est pair, alors la div est censée être grande. Donc on la rétrécit
-//         var x = document.getElementsByClassName(div);
-//         x[0].style.width="350px";
-//         x[0].style.height="350px";
-//     }
-// }
